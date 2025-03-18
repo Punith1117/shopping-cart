ce8c459b607134ed4527cbaacb4b0bb37eab701f
Punith1117 <punith11172006@gmail.com>
2025-03-18 20:25:48 +0530

    add test and code to pass the test which checks if fetch is called on initial render

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,10 +1,16 @@
-import { describe, expect, it } from "vitest";
+import { vi, describe, expect, it } from "vitest";
 import App from "../src/App";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { act } from "react";
 
 describe('App', () => {
-    it('heading should have text Vite + React', () => {
+    it('should call fetch on render to fetch products from api', () => {
+        global.fetch = vi.fn(() =>
+            Promise.resolve({
+              json: () => Promise.resolve({product: { id: 1, title: 'shirt'}}),
+            })
+        );
         render(<App></App>)
-        expect(screen.getByRole('heading').textContent).toBe('Vite + React')
+        expect(fetch).toHaveBeenCalledTimes(1)
     })
 })
