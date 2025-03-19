c62487b92c9f11b789efac398ece61683a5f78b3
Punith1117 <punith11172006@gmail.com>
2025-03-19 18:50:58 +0530

    add tests and code to display button to add or remove product from the cart
    the tests check if the buttons are displayed

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,4 +1,4 @@
-function Products({products}) {
+function Products({products, cart, isInCart}) {
     return (
         products.map(item => (
             <div className="product" data-testid="product" key={item.id}>
@@ -6,6 +6,7 @@ function Products({products}) {
                 <p>{item.title}</p>
                 <p>{item.description}</p>
                 <p>{item.price}</p>
+                { isInCart(item.id, cart) ? <button>Remove From Cart</button> : <button>Add To Cart</button> }
             </div>
         ))
     )
--- tests/Products.test.jsx
+++ tests/Products.test.jsx
@@ -1,13 +1,22 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import Products from "../src/Products";
+
+let isInCart = vi.fn(
+    (id, cart) => {
+        if (cart == null || cart == undefined) return undefined
+        if (cart.length == 0) return false
+        for(let item of cart) {
+            if (item.id == id) return true
+        }
+})
 
 describe('Products', () => {
     it('should display all the products', () => {
         let products = [
             { id: 1 }, { id: 2 }, { id: 3 }
         ]
-        render(<Products products={products}></Products>)
+        render(<Products products={products} isInCart={isInCart}></Products>)
         let displayedProducts = screen.getAllByTestId('product')
         expect(displayedProducts).toHaveLength(3)
     })
@@ -20,7 +29,7 @@ describe('Products', () => {
                 description: 'abcd',
             }
         ]
-        render(<Products products={products}/>)
+        render(<Products products={products} isInCart={isInCart} />)
         let title = screen.getByText('ABC')
         let price = screen.getByText('10')
         let description = screen.getByText('abcd')
@@ -30,8 +39,22 @@ describe('Products', () => {
     })
     it('should display image of the product', () => {
         let products = [ { id: 1, imageUrl: 'abcd'}]
-        render(<Products products={products}/>)
+        render(<Products products={products} isInCart={isInCart} />)
         let image = screen.getAllByTestId('product-image')[0]
         expect(image).toHaveAttribute('src', 'abcd')
+    })
+    it('should display a button to add the product to cart if the product is not in the cart', () => {
+        let products = [ {id: 1} ]
+        let cart = []
+        render(<Products products={products} cart={cart} isInCart={isInCart} />)
+        let addToCart = screen.getByRole('button', { name: 'Add To Cart' })
+        expect(addToCart).toBeInTheDocument()
+    })
+    it('should display a button to remove the product from cart if the product is in the cart', () => {
+        let products = [ {id: 1} ]
+        let cart = [ {id: 1} ]
+        render(<Products products={products} cart={cart} isInCart={isInCart} />)
+        let removeFromCart = screen.getByRole('button', { name: 'Remove From Cart' })
+        expect(removeFromCart).toBeInTheDocument()
     })
 })
