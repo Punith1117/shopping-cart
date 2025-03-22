952cb81662a4bbfb03a8210ec8a21dc08a3c86b1
Punith1117 <punith11172006@gmail.com>
2025-03-22 21:04:04 +0530

    modify return to render loading if products is null

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,14 +1,22 @@
-function Products({products, cart, isInCart, addToCart, removeFromCart}) {
+function Products({products, isInCart, addToCart, removeFromCart}) {
     return (
-        products.map(item => (
-            <div className="product" data-testid="product" key={item.id}>
-                <img src={item.imageUrl} alt="product-image" data-testid='product-image' />
-                <p>{item.title}</p>
-                <p>{item.description}</p>
-                <p>{item.price}</p>
-                { isInCart(item.id) ? <button onClick={() => removeFromCart(item.id)}>Remove From Cart</button> : <button onClick={() => addToCart(item)}>Add To Cart</button> }
-            </div>
-        ))
+        (products == null) ? (
+            <p>loading</p>
+        ) : (
+            <>
+            {
+                products.map(item => (
+                    <div className="product" data-testid="product" key={item.id}>
+                        <img src={item.imageUrl} alt="product-image" data-testid='product-image' />
+                        <p>{item.title}</p>
+                        <p>{item.description}</p>
+                        <p>{item.price}</p>
+                        { isInCart(item.id) ? <button onClick={() => removeFromCart(item.id)}>Remove From Cart</button> : <button onClick={() => addToCart(item)}>Add To Cart</button> }
+                    </div>
+                ))
+            }
+            </>
+        )
     )
 }
 
