2a8a4c579837780511d228543c48b803c0721957
Punith1117 <punith11172006@gmail.com>
2025-03-23 18:46:56 +0530

    add styles to Cart component and modify classnames and add wrappers to code

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,26 +1,31 @@
 import { Link, useOutletContext } from "react-router-dom";
-
+import { StyledProduct } from "./styles/Product.style" 
+import { StyledCart } from "./styles/Cart.style"
 function Cart() {
     const {cart, removeFromCart} = useOutletContext()
     let  totalPrice = cart.reduce((total, product) => total + product.price, 0);
 
     return(
-        <section className="cart">
-            {cart.map(product => (
-                <div key={product.id} data-testid="cart-item">
-                    <img src={product.image} alt="product-image" />
-                    <p>{product.title}</p>
-                    <p>{product.description}</p>
-                    <p>{product.price}</p>
-                    <button onClick={() => removeFromCart(product.id)}>Remove From Cart</button>
-                </div>
-            ))}
+        <StyledCart className="cart">
+            <section className="products">
+                {cart.map(product => (
+                    <StyledProduct key={product.id} data-testid="cart-item">
+                        <img src={product.image} alt="product-image" />
+                        <div>
+                            <h1 className="title">{product.title}</h1>
+                            <p className="description">{product.description}</p>
+                            <h2 className="price">{product.price}</h2>
+                            <button onClick={() => removeFromCart(product.id)} className="remove">Remove From Cart</button>
+                        </div>
+                    </StyledProduct>
+                ))}
+            </section>
             <div className="total-price-wrapper">
-                <p>Total Price:</p>
-                <p>{totalPrice}</p>
+                <h2>Total Price:</h2>
+                <h2>{totalPrice}</h2>
             </div>
             <Link to='pay'>Pay and Place Order</Link>
-        </section>
+        </StyledCart>
     )
 }
 
---
+++ src/styles/Cart.style.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const StyledCart = styled.section`
+    padding: 3rem;
+    background-color: #b1dfb1;
+    .products {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+    .total-price-wrapper {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+        margin: 1rem;
+        background-color: #66be66;
+    }
+    a {
+        display: flex;
+        justify-content: center;
+        background-color: #2e78e7;
+        border-radius: 1rem;
+        padding: 5px;
+        margin: 1rem;
+        margin-left: 6rem;
+        margin-right: 6rem;
+        font-size: 2rem;
+        color: white;
+        &:hover {
+            filter: brightness(0.7);
+        }
+    }
+`
