4cb1aa11347fcb299c96b28230761126861dc09c
Punith1117 <punith11172006@gmail.com>
2025-03-23 14:02:11 +0530

    add routes to child components

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,31 @@
 import App from "./App";
+import Default from "./Default";
+import Home from "./Home";
+import Products from "./Products";
+import Cart from "./Cart";
 
 const routes = [
     {
         path: "/",
         element: <App />,
-        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Default />
+            },
+            {
+                path: "home",
+                element: <Home />
+            },
+            {
+                path: "products",
+                element: <Products />
+            },
+            {
+                path: "cart",
+                element: <Cart />
+            },
+        ]
     },
 ];
 
